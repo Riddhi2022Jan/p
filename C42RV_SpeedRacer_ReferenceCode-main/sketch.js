a4548d16e6f6cc6abc30e811f434b1a5a046e58f
Riddhi2022Jan <98604461+Riddhi2022Jan@users.noreply.github.com>
2022-05-22 03:38:35 -1200

    Add files via upload

---
+++ C42RV_SpeedRacer_ReferenceCode-main/sketch.js
@@ -0,0 +1,50 @@
+var canvas;
+var backgroundImage, cycle1_png,cycle2_png, track;
+var fuelImage, powerCoinImage, lifeImage;
+var obstacle1Image, obstacle2Image;
+var database, gameState;
+var blastImage;
+var form, player, playerCount;
+var allPlayers, cycle1,cycle2, fuels, powerCoins, obstacles;
+var cycle = [];
+
+function preload() {
+  backgroundImage = loadImage("./assets/background.png");
+ cycle1_png= loadImage("../assets/cycle 123.png");
+ cycle2_png= loadImage("../assets/cycle.png");
+  track = loadImage("../assets/track.jpg");
+  fuelImage = loadImage("./assets/fuel.png");
+  powerCoinImage = loadImage("./assets/goldCoin.png");
+  obstacle1Image = loadImage("./assets/obstacle1.png");
+  obstacle2Image = loadImage("./assets/obstacle2.png");
+  lifeImage = loadImage("./assets/life.png");
+  blastImage = loadImage("./assets/blast.png");
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+function draw() {
+  background(backgroundImage);
+  if (playerCount === 2) {
+    game.update(1);
+  }
+
+  if (gameState === 1) {
+    game.play();
+  }
+
+  if (gameState === 2) {
+    game.showLeaderboard();
+    game.end();
+  }
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
